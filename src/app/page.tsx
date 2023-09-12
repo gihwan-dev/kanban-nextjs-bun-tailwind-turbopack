e65776119843db1010f1aded14beeae10400888a
최기환 <rlghks3004@gmail.com>
2023-09-12 22:15:57 +0900

    email 변경 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import BodyRoot from "@/components/body/BodyRoot";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <main
+    <motion.main
       className={`w-full h-full -bg--light-grey-light-bg -border-b--lines-light pt-6 px-6 pb-12 flex flex-row overflow-x-scroll gap-6 box-border`}
     >
       <BodyRoot />
-    </main>
+    </motion.main>
   );
 }
